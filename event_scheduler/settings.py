2bf2f6c402899bff7dcb1de8beed45131640948c
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-20 23:43:04 +0530

    Update settings.py

--- event_scheduler/settings.py
+++ event_scheduler/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "event-management-backend-mve3zxs6p-kalparatnas-projects.vercel.app",
+]
+
 
 
 # Application definition
