16178633cda391cf1420a4c229a208f5422283fa
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-20 23:31:33 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
-from django.core.asgi import get_asgi_application
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_schedular.settings')
 
+from django.core.asgi import get_asgi_application
 application = get_asgi_application()
